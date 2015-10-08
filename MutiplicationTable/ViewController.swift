6c62740beb6929a2e4d574aea3168ea76318bcef
Chris Archibald <carchi8py@gmail.com>
2015-10-07 21:27:29 -0700

    Planing on what we are going to do.

--- MutiplicationTable/ViewController.swift
+++ MutiplicationTable/ViewController.swift
@@ -13,13 +13,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //set UI in code
+        createMultiplicationTableUI()
+    }
+    
+    func createMultiplicationTableUI() {
+        
+        // Size Information (Screen Size?)
+        print("bounds: \(UIScreen.mainScreen().bounds)")
+        print("scale: \(UIScreen.mainScreen().scale)")
+        
+        // Width
+        // height
+        
+        // text label
+        
+        // view behind text label
+        
+        // Slider View
+        
+        // Uislider
+        
+        // text view
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
